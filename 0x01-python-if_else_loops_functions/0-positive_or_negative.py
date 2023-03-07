cdb9271aab158b3d424788d3356dd85da0145019
HalleluyahBoy <118768616+HalleluyahBoy@users.noreply.github.com>
2023-03-07 07:34:46 +0100

    Create 0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)
+
+
+def assign():
+  print(number)
+  if number > 0:
+    print("is positive")
+  elif number == 0:
+    print("is zero")
+  elif number < 0:
+    print("is negative")
+  else:
+    print(number)
+
+
+assign()
